26230858ecdab83587a5742a2b35740b5f532815
NaregM <nareg.mirzatuny@gmail.com>
2025-04-19 00:26:11 -0700

    Adding more to env

--- catmouseenv.py
+++ catmouseenv.py
@@ -20,4 +20,24 @@ class CatMouseEnv:
     def reset(self) -> npt.NDArray:
         """
         """
-        return None
+        if self.mode == 'static':
+            
+            # For 'static' mode, cat and mouse start from a preset position
+            self.cat   = Cat(position=0)
+            self.mouse = Mouse(position=4)
+            
+        elif self.mode == "random":
+            # For 'random' node cat and mouse are assigned initial loc on random 
+            self.cat = Cat(position=np.random.choice(self.nodes))
+            self.mouse = Mouse(position=np.random.choice([n for n in self.nodes if n!=self.cat.position]))
+            
+        else:
+            
+            raise ValueError("Mode must be either 'static' or 'random'!")
+        
+        self.steps = 0
+        return self._get_state()
+    
+    def _get_state(self) -> npt.NDArray:
+        
+        return
